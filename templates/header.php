3ab6b8cf251db1e52ae63558f6faecee7b23eadc
behzad <behzad.kermanii@gmail.com>
2023-06-25 08:28:55 +0330

    delete file

---
+++ templates/header.php
@@ -0,0 +1,23 @@
+<div>
+    <div id="page-content-wrapper">
+        <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
+            <div class="d-flex align-items-center">
+                <i class="fs-4 me-3" id="menu-toggle">
+                    <ion-icon name="menu-outline"></ion-icon>
+                </i>
+                <h2 class="fs-2 m-o"> سامانه حسابداری </h2>
+            </div>
+
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item-dropdown">
+                        <a href="#" class="nav-link dropdown-toggle second-text fw-bold" id="navbarDropdown"
+                            role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i>فاطمه رزوان</i>
+                        </a>
+
+                    </li>
+                </ul>
+            </div>
+        </nav>
